b11ab8b5898e51cdf152a0d0d12f662f1f606477
nickimariecarr <ncarr1@live.maryville.edu>
2021-08-10 23:45:47 -0400

    assignment upload

---
+++ src/app/groceries-service.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+
+@Injectable({
+	providedIn: 'root'
+})
+
+export class GroceriesServiceService {
+  items: any = [];
+  object: any = [];
+  
+  dataChanged$ : Observable<boolean>;
+
+  private dataChangeSubject: Subject<boolean>;
+
+  baseURL = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {
+    console.log('Hello GroceriesService');
+    
+    this.dataChangeSubject = new Subject<boolean>();
+    this.dataChanged$ = this.dataChangeSubject.asObservable();
+  }
+
+  getItems(): Observable<Object[]> {
+    return this.http.get(this.baseURL + '/api/groceries').pipe( map(this.extractData), catchError(this.handleError)
+    );
+  }
+
+  private extractData(res: Response){
+    let body = res;
+    return (body || {}) as object[];
+  }
+
+  private handleError(error: Response | any){
+    let errMsg : string;
+    if(error instanceof Response){
+      const err = error || '';
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    }else{
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+ 
+  removeItem(index: string){
+    console.log(index);
+    this.http.delete(this.baseURL+'/api/groceries/'+index).subscribe(res => {
+      this.items = res;
+      this.dataChangeSubject.next(true);
+    });
+  }
+ 
+  addItem(item: any){
+    this.http.post(this.baseURL+'/api/groceries/', item).subscribe(res => {
+      this.items = res;
+      this.dataChangeSubject.next(true);
+    });
+  }
+  
+  editItem(item: string, index: string | number){
+    this.http.put(this.baseURL+'/api/groceries/'+ item, index).subscribe(res => {
+      this.items[index] = res;
+      this.dataChangeSubject.next(true);
+    });
+  }}
---
+++ src/app/input-dialog-service.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { GroceriesServiceService } from './groceries-service.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InputDialogServiceService {
+
+  async showAlert(item?, index?) {
+    const alert = await this.alertCtrl.create({
+      header: item ? 'Edit Item' : 'Add Item',
+      message: item ? 'Please edit item...' : "Please enter item...",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name',
+          value: item ? item.name : null
+        },
+        {
+          name: 'quantity',
+          type: 'text',
+          placeholder: 'Quantity',
+          value: item ? item.quantity : null
+        }
+      ],
+
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log('Save Handler', data);
+            if (index !== undefined) {
+              item.name = data.name;
+              item.quantity =data.quantity;
+              this.dataService.editItem(item, index);
+            }
+            else {
+              this.dataService.addItem(item);
+            }
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+  constructor(public dataService: GroceriesServiceService, public alertCtrl: AlertController) { }
+}
